fe9e4c816f9e2a141497a58b58d7fc7b3ebfd3ff
Brenthubbard <76464105+Brenthubbard@users.noreply.github.com>
2021-07-09 13:17:56 -0600

    all tests are passing now

--- taproom/src/__tests__/reducers/tapRoom-reducer-test.js
+++ taproom/src/__tests__/reducers/tapRoom-reducer-test.js
@@ -3,22 +3,33 @@ import tapRoomReducer from '../../reducers/tapRoom-reducer';
 
 describe('tapRoom-reducer', () => {
 
+  const currentState = {
+  
+      1: {
+        name: 'Kokanee',
+        brand: 'Super awesome',
+        price: 5,
+        alcoholContent: 15,
+        id: 1
+      },
+      2: {
+        name: 'Kokanee Gold',
+        brand: 'WOW! awesome',
+        price: 12,
+        alcoholContent: 35,
+        id: 2
+      }
+    };
+  
   let action;
   const kegData = {
-    1: {
       name: 'Kokanee',
       brand: 'Super awesome',
       price: 5,
       alcoholContent: 15,
       id: 1
-    },
-    2: {
-      name: 'Kokanee Gold',
-      brand: 'WOW! awesome',
-      price: 12,
-      alcoholContent: 35,
-      id: 2
-    }
+    
+    
   };
 
   test('should return default state if there is no action type passed into the reducer', () => {
@@ -50,7 +61,7 @@ describe('tapRoom-reducer', () => {
   });
 
 
-  
+
   test('Should successfully delete a Keg', () => {
     action = {
       type: 'DELETE_KEG',
