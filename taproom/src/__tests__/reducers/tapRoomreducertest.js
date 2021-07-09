2ef577dee4ca3e4c738daef0d66317dacd703bd8
Brenthubbard <76464105+Brenthubbard@users.noreply.github.com>
2021-07-09 11:03:41 -0600

    still trying to get this first to work

--- taproom/src/__tests__/reducers/tapRoomreducertest.js
+++
@@ -1,7 +0,0 @@
-import tapRoom-reducer from '../../reducers/tapRoom-reducer';
-
-describe('tapRoom-reducer', () => {
-  test('should returdefault state if there is no action type passed into the reducer', () => {
-    expect(tapRoom - reducer({}, { type: null })).toEqual({});
-  });
-});
