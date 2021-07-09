d056566dc93493dfea8628a337358b0cef485fc2
Brenthubbard <76464105+Brenthubbard@users.noreply.github.com>
2021-07-09 11:21:14 -0600

    first failing test

--- taproom/src/__tests__/reducers/tapRoom-reducer-test.js
+++ taproom/src/__tests__/reducers/tapRoom-reducer-test.js
@@ -1,7 +1,8 @@
-import tapRoom-reducer from '../../reducers/tapRoom-reducer';
+import tapRoomReducer from '../../reducers/tapRoom-reducer';
+
 
 describe('tapRoom-reducer', () => {
   test('should return default state if there is no action type passed into the reducer', () => {
-    expect(tapRoom-reducer({}, { type: null })).toEqual({});
+    expect(tapRoomReducer({}, { type: null })).toEqual({});
   });
 });
